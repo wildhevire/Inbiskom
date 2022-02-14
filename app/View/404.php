5217bfe9da4acd75cecde6334152c4e10bbd4f5b
BennoAlif <bennoalif41@gmail.com>
2022-02-14 19:32:47 +0700

    Page 404

--- app/View/404.php
+++ app/View/404.php
@@ -1,3 +1,8 @@
-<h1>
-    405
-</h1>
+<div class="text-center mt-5">
+    <h4>
+        Produk atau Toko yang anda cari tidak ditemukan, kembali ke <a href="/">halaman utama</a>
+    </h4>
+    
+    <img src="/assets/404.png" alt="not found" width="640">
+
+</div>
