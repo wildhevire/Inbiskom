326be30823135097bb5b49fb6b51bf3db9b55214
BennoAlif <bennoalif41@gmail.com>
2022-02-21 10:47:59 +0000

    Update from vps

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -27,13 +27,13 @@ class Router
         if (isset($_SERVER['PATH_INFO'])) {
             $path = $_SERVER['PATH_INFO'];
         }
+die();
 
         $method = $_SERVER['REQUEST_METHOD'];
 
         foreach (self::$routes as $route) {
             $pattern = "#^" . $route['path'] . "$#";
             if (preg_match($pattern, $path, $variables) && $method == $route['method']) {
-
                 // call middleware
                 foreach ($route['middleware'] as $middleware){
                     $instance = new $middleware;
--- config/database.php
+++ config/database.php
@@ -5,14 +5,14 @@ function DatabaseConfig(): array
     return [
         "database" => [
             "prod" => [
-                "url" => "mysql:host=localhost:8079;dbname=inbiskom",
-                "username" => "root",
-                "password" => "root"
+                "url" => "mysql:host=127.0.0.1:8079;dbname=inbiskom_test",
+                "username" => "inbiskom",
+                "password" => "inbiskom##89"
             ],
             "test" => [
-                "url" => "mysql:host=localhost:8079;dbname=inbiskom_test",
-                "username" => "root",
-                "password" => "root"
+                "url" => "mysql:host=127.0.0.1:8079;dbname=inbiskom_test",
+                "username" => "inbiskom",
+                "password" => "inbiskom##89"
             ]
         ]
     ];
